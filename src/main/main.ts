f6a42cd97d46ed2f20b357f178f89ac321ec2cec
Jono Sachs <jonosachs@gmail.com>
2026-06-18 10:19:10 +1000

    fix: Comment out openDevTools from main

--- src/main/main.ts
+++ src/main/main.ts
@@ -21,8 +21,8 @@ const createWindow = () => {
   // and load the index.html of the app.
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // Open the DevTools (DEV only).
+  // mainWindow.webContents.openDevTools();
 };
 
 // Register named request 'get-markdown'
